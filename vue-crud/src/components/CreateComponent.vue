6d4ca5a085270e31ef3aa60c731864dd9e5b5589
mehmetalp <mehmetakifalp@gmail.com>
2019-04-21 21:09:30 +0300

    create component

--- vue-crud/src/components/CreateComponent.vue
+++ vue-crud/src/components/CreateComponent.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Create Component</div>
-
-              <div class="card-body">
-                  I'm the Create Component component.
-              </div>
+  <div>
+    <h1>Create A Post</h1>
+    <form @submit.prevent="addPost">
+      <div class="row">
+        <div class="col-md-6">
+          <div class="form-group">
+            <label>Post Title:</label>
+            <input type="text" class="form-control" v-model="post.title">
           </div>
-      </div>
+        </div>
+        </div>
+        <div class="row">
+          <div class="col-md-6">
+            <div class="form-group">
+              <label>Post Body:</label>
+              <textarea class="form-control" v-model="post.body" rows="5"></textarea>
+            </div>
+          </div>
+        </div><br />
+        <div class="form-group">
+          <button class="btn btn-primary">Create</button>
+        </div>
+    </form>
   </div>
 </template>
 
 <script>
-  export default {
+    export default {
+        data(){
+        return {
+          post:{}
+        }
+    },
+    methods: {
+      addPost(){
+        console.log(this.post);
+      }
+    }
   }
 </script>
