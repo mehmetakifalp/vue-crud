1788a057df96bc493a7797001261967bba494f66
mehmetalp <mehmetakifalp@gmail.com>
2019-04-22 20:52:30 +0300

    create component and index component

--- vue-crud/src/components/CreateComponent.vue
+++ vue-crud/src/components/CreateComponent.vue
@@ -34,7 +34,10 @@
     },
     methods: {
       addPost(){
-        console.log(this.post);
+      let uri = 'http://localhost:4000/posts/add';
+      this.axios.post(uri, this.post).then(() => {
+        this.$router.push({name: 'posts'});
+      })
       }
     }
   }
--- vue-crud/src/components/IndexComponent.vue
+++ vue-crud/src/components/IndexComponent.vue
@@ -1,18 +1,45 @@
 <template>
-  <div class="row justify-content-center">
-      <div class="col-md-8">
-          <div class="card card-default">
-              <div class="card-header">Index Component</div>
-
-              <div class="card-body">
-                 My products table.
-              </div>
+  <div>
+      <h1>Posts</h1>
+        <div class="row">
+          <div class="col-md-10"></div>
+          <div class="col-md-2">
+            <router-link :to="{ name: 'create' }" class="btn btn-primary">Create Post</router-link>
           </div>
-      </div>
+        </div><br />
+
+        <table class="table table-hover">
+            <thead>
+            <tr>
+              <th>Title</th>
+              <th>Body</th>
+              <th>Actions</th>
+            </tr>
+            </thead>
+            <tbody>
+                <tr v-for="post in posts" :key="post._id">
+                  <td>{{ post.title }}</td>
+                  <td>{{ post.body }}</td>
+                  <td><router-link :to="{name: 'edit', params: { id: post._id }}" class="btn btn-primary">Edit</router-link></td>
+                  <td><button class="btn btn-danger">Delete</button></td>
+                </tr>
+            </tbody>
+        </table>
   </div>
 </template>
 
 <script>
   export default {
+      data() {
+        return {
+          posts: []
+        }
+      },
+      created() {
+      let uri = 'http://localhost:4000/posts';
+      this.axios.get(uri).then(response => {
+        this.posts = response.data;
+      });
+    }
   }
 </script>
